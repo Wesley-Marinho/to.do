840628dcb579602541148197d5cba0c8a0ced406
Wesley Marinho <51543121+Wesley-Marinho@users.noreply.github.com>
2021-08-24 12:58:30 -0300

    Versão final

---
+++ src/__tests__/components/TaskList.spec.tsx
@@ -0,0 +1,131 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import React from 'react';
+import { TaskList } from '../../components/TaskList';
+
+describe('App Page', () => {
+  it('should be able to add a task', async () => {
+    render(<TaskList />);
+
+    const taskInput = screen.getByPlaceholderText('Adicionar novo todo');
+    const addTaskButton = screen.getByTestId('add-task-button');
+
+    fireEvent.change(taskInput, {
+      target: {
+        value: 'Desafio Comeia'
+      }
+    });
+    fireEvent.click(addTaskButton);
+
+    const addedFirstTaskTitle = screen.getByText('Desafio Comeia');
+
+    expect(addedFirstTaskTitle).toHaveTextContent('Desafio Comeia');
+    expect(addedFirstTaskTitle.parentElement).not.toHaveClass('completed')
+
+    fireEvent.change(taskInput, {
+      target: {
+        value: 'Estudar'
+      }
+    });
+    fireEvent.click(addTaskButton);
+
+    const addedSecondTaskTitle = screen.getByText('Estudar');
+
+    expect(addedFirstTaskTitle).toBeInTheDocument();
+    expect(addedFirstTaskTitle).toHaveTextContent('Desafio Comeia');
+    expect(addedFirstTaskTitle.parentElement).not.toHaveClass('completed')
+
+    expect(addedSecondTaskTitle).toHaveTextContent('Estudar');
+    expect(addedSecondTaskTitle.parentElement).not.toHaveClass('completed')
+  })
+
+  it('should not be able to add a task with a empty title', () => {
+    render(<TaskList />);
+
+    const addTaskButton = screen.getByTestId('add-task-button');
+
+    fireEvent.click(addTaskButton);
+
+    expect(screen.queryByTestId('task')).not.toBeInTheDocument();
+
+    const taskInput = screen.getByPlaceholderText('Adicionar novo todo');
+
+    fireEvent.change(taskInput, {
+      target: {
+        value: 'Desafio Comeia'
+      }
+    });
+    
+    fireEvent.click(addTaskButton);
+
+    const addedFirstTaskTitle = screen.getByText('Desafio Comeia');
+
+    expect(addedFirstTaskTitle).toHaveTextContent('Desafio Comeia');
+  })
+
+  it('should be able to remove a task', async () => {
+    render(<TaskList />);
+
+    const taskInput = screen.getByPlaceholderText('Adicionar novo to do');
+    const addTaskButton = screen.getByTestId('add-task-button');
+
+    fireEvent.change(taskInput, {
+      target: {
+        value: 'Desafio Comeia'
+      }
+    });
+    fireEvent.click(addTaskButton);
+
+    fireEvent.change(taskInput, {
+      target: {
+        value: 'Estudar'
+      }
+    });
+    fireEvent.click(addTaskButton);
+
+    const addedFirstTaskTitle = screen.getByText('Desafio Comeia');
+    const addedSecondTaskTitle = screen.getByText('Estudar');
+
+    expect(addedFirstTaskTitle).toBeInTheDocument()
+    expect(addedSecondTaskTitle).toBeInTheDocument();
+
+    const [addedFirstTaskRemoveButton] = screen.getAllByTestId('remove-task-button');
+
+    fireEvent.click(addedFirstTaskRemoveButton);
+
+    expect(addedFirstTaskTitle).not.toBeInTheDocument();
+    expect(addedSecondTaskTitle).toBeInTheDocument();
+  })
+
+  it('should be able to check a task', () => {
+    render(<TaskList />);
+
+    const taskInput = screen.getByPlaceholderText('Adicionar novo to do');
+    const addTaskButton = screen.getByTestId('add-task-button');
+
+    fireEvent.change(taskInput, {
+      target: {
+        value: 'Desafio Comeia'
+      }
+    });
+    fireEvent.click(addTaskButton);
+
+    fireEvent.change(taskInput, {
+      target: {
+        value: 'Estudar'
+      }
+    });
+    fireEvent.click(addTaskButton);
+
+    const [addedFirstTask, addedSecondTask] = screen.getAllByTestId('task');
+
+    if (addedFirstTask.firstChild) {
+      fireEvent.click(addedFirstTask.firstChild)
+    }
+
+    expect(addedFirstTask).toBeInTheDocument();
+    expect(addedFirstTask).toHaveClass('completed');
+
+    expect(addedSecondTask).toBeInTheDocument();
+    expect(addedSecondTask).not.toHaveClass('completed');
+  })
+})
